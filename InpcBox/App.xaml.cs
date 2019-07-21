dc03bc4fa76365d181bf0c7df1ffd098e02228ed
Johan Larsson <johan_gavle1@hotmail.com>
2019-07-21 21:12:32 +0200

    Initial.

---
+++ InpcBox/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace InpcBox
+{
+    using System.Windows;
+
+    public partial class App : Application
+    {
+    }
+}
---
+++ InpcBox/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace InpcBox
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Value { get; set; }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
